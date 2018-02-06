8000b3cee82dde167db40a2a24568112d4d22071
Jon Wilson <2jonwilson@gmail.com>
2018-02-05 17:10:11 -0800

    greatly improved the bubble arrangements; bubble bounce logic now accounts for strokeWeight; limited visible bubbles to the 6 family members; press +/- to add/remove bubbles from the stage and re-apply the current arrangment

--- src/Bubble.js
+++ src/Bubble.js
@@ -1,142 +1,154 @@
-var _bounceSnd;
-function Bubble(img,sounds,strokeColor,vel,w){
-  this.img = img;
-  this.snd = new CircularArray(sounds);
-  this.pos = createVector(0,0);
-  this.vel = vel;
-  this.rot = 0;
-  this.w = w;
-  this.spinPositive = true;
+window.Bubble = (function(){
+  var _ellipseDiameter = 150;
+  var _strokeWeight = 10;
+  var _outerDiameter = _ellipseDiameter + _strokeWeight;
+  var _bounceSnd;
 
-  var _doneSpinningListeners = [];
-  this.onceDoneSpinning = function(callback){
-    _doneSpinningListeners = [callback];
-  }
-  sounds.forEach(s => s.onended(donePlayingSound.bind(this)));
+  function Bubble(img,sounds,strokeColor,vel){
+    this.img = img;
+    this.snd = new CircularArray(sounds);
+    this.pos = createVector(0,0);
+    this.vel = vel;
+    this.rot = 0;
+    this.spinPositive = true;
 
-  this.draw = function(){
-    push();
-    noFill();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.rot);
-    strokeWeight(7);
-    stroke(strokeColor);
-    //position refers to the CENTER of the bubble.
-    let s = this.snd.getCurrent().isPlaying() ? this.w * 1.5 : this.w;
-    image(this.img, -s/2, -s/2, s, s);
-    ellipse(0, 0, s, s);
-    pop();
-  };
-  
-  let _activate = function(){
-    let s = this.snd.getCurrent();
-    if(keyIsDown(SHIFT)){
-      s.reverseBuffer();
-      this.spinPositive = !this.spinPositive;
-    } 
-    if(s.isPlaying()){
-      this.rot = 0;
-      s.stop();
+    var _doneSpinningListeners = [];
+    this.onceDoneSpinning = function(callback){
+      _doneSpinningListeners = [callback];
     }
-    s.setVolume(0.3);
-    s.play();
-  };
+    sounds.forEach(s => s.onended(donePlayingSound.bind(this)));
 
-  function donePlayingSound(){
-    console.log('done playing sound');
-    _doneSpinningListeners.forEach(l => l());
-    _doneSpinningListeners.splice(0);
-    this.snd.next();
-  }
-
-  this.touchStarted = _activate.bind(this);
-  this.touched = _activate.bind(this);
-
-
-  this.impactFrom = function(impactPos){
-    let funFactor = 20000;
-    let impactOffset = p5.Vector.sub(impactPos, this.pos);
-    let d = impactOffset.mag();
-    let intensity = 1 / (d * d); //Source: https://www.nde-ed.org/EducationResources/CommunityCollege/Radiography/Physics/inversesquare.htm
-    let deltaV = impactOffset.normalize().mult(intensity * funFactor);
-    let inverse = createVector(-deltaV.x, -deltaV.y);
-    this.vel = p5.Vector.add(this.vel, inverse);
-  };
-
-  this.containsPoint = function(x,y){
-    return dist(this.pos.x, this.pos.y, x, y) <= this.w/2;
-  };
-
-  this.update = function(){
-    this.updateRotation();
-    this.updatePosition();
-  };
-
-  this.updateRotation = function(){
-    if(keyIsDown(LEFT_ARROW)){
-      this.rot -= 5;
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-      this.rot += 5;
-    }
-    if(this.snd.getCurrent().isPlaying()){
-      if(this.spinPositive){
-        this.rot += 10;
-      } else {
-        this.rot -= 10;
+    this.draw = function(){
+      push();
+      translate(this.pos.x, this.pos.y);
+      rotate(this.rot);
+      let d = this.snd.getCurrent().isPlaying() 
+        ? _ellipseDiameter * 1.5 
+        : _ellipseDiameter;
+      strokeWeight(_strokeWeight);
+      stroke(strokeColor);
+      noFill();
+      ellipseMode(CENTER);
+      ellipse(0, 0, d, d);
+      image(this.img, -d/2, -d/2, d, d);
+      pop();
+    };
+    
+    let _activate = function(){
+      let s = this.snd.getCurrent();
+      if(keyIsDown(SHIFT)){
+        s.reverseBuffer();
+        this.spinPositive = !this.spinPositive;
+      } 
+      if(s.isPlaying()){
+        this.rot = 0;
+        s.stop();
       }
-      if(this.rot >= 360){
-        this.rot = this.rot % 360;
-      } else if (this.rot < 0) {
-        this.rot += 360;
-      }
-      if(this.rot == 0){
-      }
+      s.setVolume(0.3);
+      s.play();
+    };
+
+    function donePlayingSound(){
+      console.log('done playing sound');
+      _doneSpinningListeners.forEach(l => l());
+      _doneSpinningListeners.splice(0);
+      this.snd.next();
     }
-  };
 
-  function playBounceSound(){
-      _bounceSnd.setVolume(0.05);
-      _bounceSnd.play();
-  }
+    this.touchStarted = _activate.bind(this);
+    this.touched = _activate.bind(this);
 
-  this.updatePosition = function(){
-    let c = keyIsDown(32 /* SPACE */) ? 0.25 : 1;
 
-    this.pos.x += this.vel.x * c;
-    let xOver = this.pos.x + this.w/2 - width;
-    if(xOver > 0){
-      this.vel.x = -this.vel.x;
-      this.pos.x -= (xOver + 1);
-      playBounceSound()
-    } else {
-      let xUnder = this.pos.x - this.w/2;
-      if(xUnder < 0){
+    this.impactFrom = function(impactPos){
+      let funFactor = 20000;
+      let impactOffset = p5.Vector.sub(impactPos, this.pos);
+      let d = impactOffset.mag();
+      let intensity = 1 / (d * d); //Source: https://www.nde-ed.org/EducationResources/CommunityCollege/Radiography/Physics/inversesquare.htm
+      let deltaV = impactOffset.normalize().mult(intensity * funFactor);
+      let inverse = createVector(-deltaV.x, -deltaV.y);
+      this.vel = p5.Vector.add(this.vel, inverse);
+    };
+
+    this.containsPoint = function(x,y){
+      return dist(this.pos.x, this.pos.y, x, y) <= _ellipseDiameter/2;
+    };
+
+    this.update = function(){
+      this.updateRotation();
+      this.updatePosition();
+    };
+
+    this.updateRotation = function(){
+      if(keyIsDown(LEFT_ARROW)){
+        this.rot -= 5;
+      }
+      if(keyIsDown(RIGHT_ARROW)){
+        this.rot += 5;
+      }
+      if(this.snd.getCurrent().isPlaying()){
+        if(this.spinPositive){
+          this.rot += 10;
+        } else {
+          this.rot -= 10;
+        }
+        if(this.rot >= 360){
+          this.rot = this.rot % 360;
+        } else if (this.rot < 0) {
+          this.rot += 360;
+        }
+        if(this.rot == 0){
+        }
+      }
+    };
+
+    function playBounceSound(){
+        _bounceSnd.setVolume(0.05);
+        _bounceSnd.play();
+    }
+
+    this.updatePosition = function(){
+      let c = keyIsDown(32 /* SPACE */) ? 0.25 : 1;
+      let ow = Bubble.diameter();
+
+      this.pos.x += this.vel.x * c;
+      let xOver = this.pos.x + ow/2 - width;
+      if(xOver > 0){
         this.vel.x = -this.vel.x;
-        this.pos.x -= (xUnder - 1);
+        this.pos.x -= (xOver + 1);
         playBounceSound()
+      } else {
+        let xUnder = this.pos.x - ow/2;
+        if(xUnder < 0){
+          this.vel.x = -this.vel.x;
+          this.pos.x -= (xUnder - 1);
+          playBounceSound()
+        }
       }
-    }
 
-    this.pos.y += this.vel.y * c;
-    let yOver = this.pos.y + this.w/2 - height;
-    if(yOver >= 0){
-      this.vel.y = -this.vel.y;
-      this.pos.y -= (yOver + 1);
-      playBounceSound()
-    } else {
-      let yUnder = this.pos.y - this.w/2;
-      if(yUnder < 0){
+      this.pos.y += this.vel.y * c;
+      let yOver = this.pos.y + ow/2 - height;
+      if(yOver >= 0){
         this.vel.y = -this.vel.y;
-        this.pos.y -= (yUnder - 1);
+        this.pos.y -= (yOver + 1);
         playBounceSound()
+      } else {
+        let yUnder = this.pos.y - ow/2;
+        if(yUnder < 0){
+          this.vel.y = -this.vel.y;
+          this.pos.y -= (yUnder - 1);
+          playBounceSound()
+        }
       }
-    }
+    };
+  }
+
+  Bubble.diameter = function(){
+    return _outerDiameter;
   };
-}
 
-Bubble.setup = function(){
-  _bounceSnd = loadSound("../assets/sounds/impactcrash-basketball-bounce-wood-surface-thud-blastwavefx-29503.mp3");
-}
+  Bubble.setup = function(){
+    _bounceSnd = loadSound("../assets/sounds/impactcrash-basketball-bounce-wood-surface-thud-blastwavefx-29503.mp3");
+  }
 
-function BubbleState(){}
+  return Bubble;
+})();
--- src/sketch.js
+++ src/sketch.js
@@ -1,11 +1,13 @@
 
 /*jshint esversion: 6 */
 var bubbles = [];
+var unusedBubbles = [];
 var backgrounds = [];
 var bg;
 var playing = false;
 var touchColor;
 var arrangements = [];
+var currentArrangement;
 function setup() {
   colorMode(HSL, 255, 255, 255, 100);
   touchColor = color(0,0,0,50);
@@ -24,18 +26,8 @@ function setup() {
   var mildVelocity = function(v){
     return v.normalize().mult(0.5);
   };
-  bubbles = [
-    new Bubble(
-      loadImage("../assets/sawyer1.png"),
-      [
-        loadSound("../assets/sounds/sawyer-by-papa.wav"),
-        loadSound("../assets/sounds/voice-prompts-greetings-greetings-child-boy-hi-human-voice-speaking-2.mp3"),
-        loadSound("../assets/sounds/other-human-girl-laughs-4-times-faster-and-higher-nightingale-music-productions-12449.mp3"),
-        loadSound("../assets/sounds/voice-prompts-pain-pain-child-girl-ouch-human-voice.mp3")
-      ],
-      color(170,255,148),
-      mildVelocity(createVector(4,0)),
-      w),
+  ellipseMode(CENTER);
+  unusedBubbles = [
     new Bubble(loadImage("../assets/mama1.png"),
       [
         //"mama-hello-1.wav",
@@ -47,8 +39,7 @@ function setup() {
         "mama-buh-bye.wav"
       ].map(fn => loadSound("../assets/sounds/" + fn)),
       color(238,255,148),
-      mildVelocity(createVector(2,2)),
-      w),
+      mildVelocity(createVector(2,2))),
     new Bubble(loadImage("../assets/papa1.png"),
       [
         "papa-by-papa.wav",
@@ -56,8 +47,24 @@ function setup() {
         "papa-buh-bye-1.wav"
       ].map(fn => loadSound("../assets/sounds/" + fn)),
       color(102,55,148),
-      mildVelocity(createVector(2,-2)),
-      w),
+      mildVelocity(createVector(2,-2))),
+    new Bubble(
+      loadImage("../assets/sawyer1.png"),
+      [
+        loadSound("../assets/sounds/sawyer-by-papa.wav"),
+        loadSound("../assets/sounds/voice-prompts-greetings-greetings-child-boy-hi-human-voice-speaking-2.mp3"),
+        loadSound("../assets/sounds/other-human-girl-laughs-4-times-faster-and-higher-nightingale-music-productions-12449.mp3"),
+        loadSound("../assets/sounds/voice-prompts-pain-pain-child-girl-ouch-human-voice.mp3")
+      ],
+      color(170,255,148),
+      mildVelocity(createVector(4,0))),
+    new Bubble(loadImage("../assets/fetus1.png"),
+      [
+        loadSound("../assets/sounds/baby-by-papa.wav"),
+        loadSound("../assets/sounds/fetus-1.wav")
+      ],
+      color(43,255,148),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/maizy1.png"),
       [
         loadSound("../assets/sounds/maizy-by-papa.wav"),
@@ -65,24 +72,14 @@ function setup() {
         //loadSound("../assets/sounds/speech-girl-says-huh-as-if-unsure-higher-nightingale-music-productions-12446.mp3")
       ],
       color(43,255,148),
-      mildVelocity(createVector(3,1)),
-      w),
+      mildVelocity(createVector(3,1))),
     new Bubble(loadImage("../assets/cooper1.png"),
       [
         loadSound("../assets/sounds/cooper-by-papa.wav"),
         loadSound("../assets/sounds/dogs-dog-bark-springer-spaniel-1.mp3")
       ],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
-    new Bubble(loadImage("../assets/fetus1.png"),
-      [
-        loadSound("../assets/sounds/baby-by-papa.wav"),
-        loadSound("../assets/sounds/fetus-1.wav")
-      ],
-      color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/pasta1.png"),
       [loadSound("../assets/sounds/pasta-1.wav")],
       color(43,255,148),
@@ -91,8 +88,7 @@ function setup() {
     new Bubble(loadImage("../assets/water3.png"),
       [loadSound("../assets/sounds/water-1.wav")],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/salsa2.png"),
       [loadSound("../assets/sounds/salsa-1.wav")],
       color(43,255,148),
@@ -101,15 +97,13 @@ function setup() {
     new Bubble(loadImage("../assets/gorilla1.png"),
       [loadSound("../assets/sounds/gorilla-by-papa.wav")],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/monkey1.png"),
       [
         loadSound("../assets/sounds/monkey-by-papa.wav"),
       ],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/elephant1.png"),
       [loadSound("../assets/sounds/elephant-by-papa.wav")],
       color(43,255,148),
@@ -118,34 +112,41 @@ function setup() {
     new Bubble(loadImage("../assets/koala1.png"),
       [loadSound("../assets/sounds/koala-by-papa.wav")],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/turtle1.png"),
       [loadSound("../assets/sounds/turtle-by-papa.wav")],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w),
+      mildVelocity(createVector(-1.5,2.5))),
     new Bubble(loadImage("../assets/lion1.png"),
       [loadSound("../assets/sounds/lion-by-papa.wav")],
       color(43,255,148),
-      mildVelocity(createVector(-1.5,2.5)),
-      w)
+      mildVelocity(createVector(-1.5,2.5)))
     ];
+
+  bubbles = [];
+  for(var i=0; i<6; i++){
+    bubbles.push(unusedBubbles.shift());
+  }
 
   arrangements = [
     (count, size) => {
-      var w = width - size;
-      var h = height - size;
-      return range(1,count).map(n => createVector(
-        n*w/count - size/2,
-        n*h/count));
+      let r = size/2;
+      let xOffset = Math.floor((width  - size)/Math.max(count - 1, 1));
+      let yOffset = Math.floor((height - size)/Math.max(count - 1, 1));
+      return range(0,count).map(n => {
+        let pos = createVector(
+          n*xOffset + r,
+          n*yOffset + r);
+        //console.log(`width:${width}, height:${height}, r:${r}, pos.x:${pos.x}, pos.y:${pos.y}, bottom-right-corner.x:${pos.x + r}, bottom-right-corner.y:${pos.y + r}`);
+        return pos;
+      });
     },
     (count,size) => {
-      var w = width - size;
-      var h = height - size;
-      var dy = h/count;
-      var xs = range(1,count).map(n => n*w/count - size/2);
-      var ys = range(count,1).map(n => n*h/count);
+      let r = size/2;
+      var dx = Math.floor((width  - size)/Math.max(count - 1, 1));
+      var dy = Math.floor((height - size)/Math.max(count - 1, 1));
+      var xs = range(0,count).map(n => n*dx + r);
+      var ys = range(count-1,0).map(n => n*dy + r);
       return zipWith(xs, ys, createVector);
     },
     (count,size) => {
@@ -155,30 +156,53 @@ function setup() {
       return chevronArrangement(count,size,false);
     },
     (count, size) => {
-      var p = 100;
-      var px = width / count;
-      var py = height / count;
-      return [
-        //This arrangement assumes count == 5 .
-        createVector(width / 2  , height / 2),
-        createVector(px         , py),
-        createVector(width - px , py),
-        createVector(px         , height - py),
-        createVector(width - px , height - py)
+      let rowConfigurations = [
+        [],
+        [1],
+        [2],
+        [3],
+        [2,2],
+        [3,2],
+        [3,3],
+        [2,3,2],
+        [3,2,3],
+        [3,3,3],
+        [3,4,3],
+        [4,3,4],
+        [4,4,4],
+        [4,5,4],
+        [5,4,5],
+        [5,5,5]
       ];
-    },
+      // Important: It is assumed that `count` will be between 0 and 15!
+      let rowConfig = rowConfigurations[count];
+      let yOffset = Math.floor(height/(rowConfig.length + 1));
+      let positions = [];
+      rowConfig.forEach((rowSize,rowIndex) => {
+        let y = yOffset * (rowIndex + 1);
+        let xOffset = Math.floor(width/(rowSize + 1));
+        for(var xIndex=1; xIndex <= rowSize; xIndex++){
+          let x = xOffset * xIndex;
+          positions.push(createVector(x,y));
+        }
+      });
+      return positions;
+    }
   ];
-
-  applyArrangement(arrangements[0]);
+  currentArrangement = arrangements[4];
+  applyArrangement(currentArrangement);
 
   function chevronArrangement(count,size,positive){
+    var dx = Math.floor((width  - size)/Math.max(count - 1, 1));
+    let r = size / 2;
     var w = width - size;
     var h = height - size;
-    var xs = range(1,count).map(n => n*w/count - size/2);
+    var xs = range(0,count).map(n => n*dx + r);
     var half = Math.floor(count / 2);
     var rows = count % 2 == 0 ? half : half + 1;
-    var _ys1 = range(1,half+1).map(n => {
-      var yy = n * h / rows - size/2
+    var dy = Math.floor((height - size)/Math.max(rows - 1, 1));
+    var _ys1 = range(0,half).map(n => {
+      var yy = n * dy + r;
       return positive ? yy : height - yy;
     });
     var _ys2 = _ys1.slice(0,-1).reverse();
@@ -191,7 +215,14 @@ function setup() {
 }
 
 function applyArrangement(arrangement){
-  zip(bubbles, arrangement(bubbles.length,150)).forEach(pair => pair[0].pos = pair[1]);
+  zip(
+    bubbles,
+    arrangement(
+      bubbles.length,
+      Bubble.diameter()))
+  .forEach(pair => {
+    pair[0].pos = pair[1];
+  });
 }
 
 function draw() {
@@ -246,17 +277,27 @@ function handlePointAction(point){
 }
 
 function keyPressed(){
-  if(49 <= keyCode && keyCode <= (49 + 9)){
-    var index = keyCode - 49;
-    if(index < arrangements.length){
-      applyArrangement(arrangements[index]);
+  let arrangementIndex = parseInt(key);
+  if(!isNaN(arrangementIndex)){
+    if(arrangementIndex < arrangements.length){
+      currentArrangement = arrangements[arrangementIndex];
+      applyArrangement(currentArrangement);
     }
   }
-  if(keyCode == 78 /* N */){
+  console.log('key:',key, 'keyCode:', keyCode);
+  if(key == 'N'){
     bubbles.forEach(b => b.vel = b.vel.normalize());
   }
-  if(keyCode == 82 /* R */){
+  if(key == 'R'){
     bubbles.forEach(b => b.rot = 0);
+  }
+  if(keyCode == 187 /* + */ && unusedBubbles.length > 0){
+    bubbles.push(unusedBubbles.shift());
+    applyArrangement(currentArrangement);
+  }
+  if(keyCode == 189 /* - */ && bubbles.length > 0){
+    unusedBubbles.unshift(bubbles.pop());
+    applyArrangement(currentArrangement);
   }
   let boostFactor = 1.25;
   if(keyCode == ESCAPE){
