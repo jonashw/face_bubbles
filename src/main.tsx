dc5f7dea5d67f30d6d19ea7d2311318d0ba87f29
Jon Wilson <2jonwilson@gmail.com>
2024-01-14 03:01:05 -0800

    simplify styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './main.scss'
 import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Sketch, {SketchLoader} from './Sketch.tsx';
 import { AppContextProvider} from './AppContext';
